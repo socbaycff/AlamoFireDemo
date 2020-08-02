e1c698439ab51d386c2d44175adfd6011faaf002
lkhang <socbaycff@gmail.com>
2020-08-02 20:15:10 +0700

    Initial Commit

---
+++ AlamoFireDemo/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  AlamoFireDemo
+//
+//  Created by L Khang on 8/2/20.
+//  Copyright © 2020 L Khang. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
